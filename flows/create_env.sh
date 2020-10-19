c9d80d5ecc59dced4ef0b9c35705a1348d184115
Nuwan Waidyanatha <waidyanatha@gmail.com>
2020-10-19 14:03:34 +0800

    first cut of the node-red flows

---
+++ flows/create_env.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+cd /home/nuwan/workspace/seismo
+ls -l
+python3 -m venv testenv
+source testenv/bin/activate
+ls -l
+which python#!/bin/bash
+
+cd /home/nuwan/workspace/seismo
+ls -l
+python3 -m venv testenv
+source testenv/bin/activate
+ls -l
+which python#!/bin/bash
+
+cd /home/nuwan/workspace/seismo
+ls -l
+python3 -m venv testenv
+source testenv/bin/activate
+ls -l
+which python
---
+++ flows/install_py_packages.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd /home/nuwan/workspace/seismo/testenv/
+ls -l
+python3 -m pip install numpy
+python3 -m pip list | grep numpy#!/bin/bash
+
+cd /home/nuwan/workspace/seismo/testenv/
+ls -l
+python3 -m pip install numpy
+python3 -m pip list | grep numpy
